b8335ca80c5e7f2bdc6cde8d7808ffba2f1d3cbf
Cam Phan <cam.phan12993@gmail.com>
2020-07-08 17:04:14 +0700

    add config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,44 @@
+const terser = require('terser');
+const htmlmin = require('html-minifier');
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+
 module.exports = function (config) {
+  if (process.env.NODE_ENV == 'production') {
+    config.addTransform('htmlmin', function (content, outputPath) {
+      if (outputPath.endsWith('.html')) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        });
+        return minified;
+      }
+
+      return content;
+    });
+  }
+
+  config.addPlugin(lazyImagesPlugin, {
+    transformImgPath: (imgPath) => {
+      if (imgPath.startsWith('/') && !imgPath.startsWith('//')) {
+        return `./src${imgPath}`;
+      }
+
+      return imgPath;
+    },
+  });
+
+  // minify js
+  config.addFilter('jsmin', function (code) {
+    let minified = terser.minify(code);
+    if (minified.error) {
+      console.log('Terser error: ', minified.error);
+      return code;
+    }
+
+    return minified.code;
+  });
+
   config.addPassthroughCopy('src/assets/css');
   return {
     dir: {
