e927768beeb05b7c9a046622134231eb3c03c3c2
Cam Phan <cam.phan12993@gmail.com>
2020-07-09 16:02:27 +0700

    add prism style into file

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,7 @@ const cssnano = require('cssnano')({
 module.exports = {
   plugins: [
     // ...
+    require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
