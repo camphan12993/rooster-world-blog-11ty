d63f5cb038787d55f728e7a0ece9117248cdae25
Cam Phan <cam.phan12993@gmail.com>
2020-07-09 09:22:28 +0700

    add admin

--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -4,3 +4,12 @@ function toggleNav() {
   mainNav.classList.toggle('active');
   navBtn.classList.toggle('open');
 }
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', (user) => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
+      });
+    }
+  });
+}
