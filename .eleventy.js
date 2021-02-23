40e64058ca3516fe7cc173f0ffd6e902442d4c79
camphan12993 <cam.phan12993@gmail.com>
2021-02-23 15:03:15 +0700

    update env

--- .eleventy.js
+++ .eleventy.js
@@ -1,81 +1,82 @@
-const terser = require("terser");
-const htmlmin = require("html-minifier");
-const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
-const pluginSEO = require("eleventy-plugin-seo-tag");
-const isProd = process.env.NODE_ENV == "production";
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const cleanCSS = require("clean-css");
-const moment = require("moment");
+const terser = require('terser');
+const htmlmin = require('html-minifier');
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const pluginSEO = require('eleventy-plugin-seo-tag');
+const isProd = process.env.NODE_ENV == 'production';
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const cleanCSS = require('clean-css');
+const moment = require('moment');
 
 module.exports = function (config) {
-	if (isProd) {
-		config.addTransform("htmlmin", function (content, outputPath) {
-			if (outputPath.endsWith(".html")) {
-				let minified = htmlmin.minify(content, {
-					useShortDoctype: true,
-					removeComments: true,
-					collapseWhitespace: true,
-				});
-				return minified;
-			}
+  console.log(process.env.NODE_ENV);
+  if (isProd) {
+    config.addTransform('htmlmin', function (content, outputPath) {
+      if (outputPath.endsWith('.html')) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        });
+        return minified;
+      }
 
-			return content;
-		});
-	}
+      return content;
+    });
+  }
 
-	config.addPlugin(lazyImagesPlugin, {
-		transformImgPath: (imgPath) => {
-			if (imgPath.startsWith("/") && !imgPath.startsWith("//")) {
-				return `./src${imgPath}`;
-			}
+  config.addPlugin(lazyImagesPlugin, {
+    transformImgPath: (imgPath) => {
+      if (imgPath.startsWith('/') && !imgPath.startsWith('//')) {
+        return `./src${imgPath}`;
+      }
 
-			return imgPath;
-		},
-	});
+      return imgPath;
+    },
+  });
 
-	// minify js
-	config.addFilter("jsmin", function (code) {
-		let minified = terser.minify(code);
-		if (minified.error) {
-			console.log("Terser error: ", minified.error);
-			return code;
-		}
+  // minify js
+  config.addFilter('jsmin', function (code) {
+    let minified = terser.minify(code);
+    if (minified.error) {
+      console.log('Terser error: ', minified.error);
+      return code;
+    }
 
-		return minified.code;
-	});
+    return minified.code;
+  });
 
-	config.addFilter("cssmin", function (code) {
-		return new cleanCSS({}).minify(code).styles;
-	});
+  config.addFilter('cssmin', function (code) {
+    return new cleanCSS({}).minify(code).styles;
+  });
 
-	// date format
-	config.addFilter("date", (dateObj) => {
-		return moment(dateObj).format("ddd-MM-yyyy");
-	});
+  // date format
+  config.addFilter('date', (dateObj) => {
+    return moment(dateObj).format('ddd-MM-yyyy');
+  });
 
-	// SEO
-	config.addPlugin(pluginSEO, {
-		title: "Rooster World Blog",
-		url: "https://blog.rooster-world.com",
-		author: "Cam Phan",
-		description: "Rooster World Blog",
-		options: {
-			titleDivider: "|",
-		},
-		image: "/assets/img/3d-printing.jpg",
-	});
+  // SEO
+  config.addPlugin(pluginSEO, {
+    title: 'Rooster World Blog',
+    url: 'https://blog.rooster-world.com',
+    author: 'Cam Phan',
+    description: 'Rooster World Blog',
+    options: {
+      titleDivider: '|',
+    },
+    image: '/assets/img/3d-printing.jpg',
+  });
 
-	config.addPlugin(syntaxHighlight);
+  config.addPlugin(syntaxHighlight);
 
-	config.addPassthroughCopy("src/assets");
-	config.addPassthroughCopy("src/admin");
-	return {
-		dir: {
-			input: "src",
-			output: "public"
-		},
-		passthroughFileCopy: true,
-		templateFormats: ["html", "md", "liquid"],
-		passthroughFileCopy: true,
-	};
+  config.addPassthroughCopy('src/assets');
+  config.addPassthroughCopy('src/admin');
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['html', 'md', 'liquid'],
+    passthroughFileCopy: true,
+  };
 };
